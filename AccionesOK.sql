f9f5e4cdff09399609cb184e835da36e88ec1318
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-04-11 19:41:42 -0500

    Se empezo acciones ok

---
+++ AccionesOK.sql
@@ -0,0 +1,8 @@
+/*SELECT * FROM multimedias WHERE idDirector = 72; */
+DELETE FROM directores WHERE id = 72;
+/*SELECT * FROM multimedias WHERE idDirector = 72; */
+
+/*SELECT * FROM categorias WHERE idMultimedia = 109; */
+DELETE FROM multimedias WHERE id = 109;
+/*SELECT * FROM categorias WHERE idMultimedia = 109; */
+
