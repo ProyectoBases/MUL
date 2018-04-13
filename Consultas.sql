98e515dadb574693359af9f827b521b19bf5a4d4
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-04-13 12:08:41 -0500

    Actualice consultas, falta corregir algunas

--- Consultas.sql
+++ Consultas.sql
@@ -1,12 +1,14 @@
-/*Consultar peliculas dirigidas por una persona especifica*/
+/*Soy un usuario y me gustaria saber las peliculas dirigidas por Mike Newell, para poder ver mas del estilo de este director*/
 SELECT multimedias.nombre, categorias.nombre, multimedias.calificacion, sinopsis, premiosMultimedia.nombre
 FROM multimedias, peliculas, directores, premiosMultimedia, categorias
-WHERE multimedias.id = peliculas.idMultimedia AND multimedias.idDirector = directores.id AND premiosMultimedia.idMultimedia = multimedias.id AND categorias.idMultimedia = multimedias.id
-        AND directores.nombre = 'Mike' AND directores.apellido = 'Newell'
+WHERE multimedias.id = peliculas.idMultimedia AND multimedias.idDirector = directores.id AND premiosMultimedia.idMultimedia = multimedias.id 
+        AND categorias.idMultimedia = multimedias.id AND directores.nombre = 'Mike' AND directores.apellido = 'Newell'
 GROUP BY multimedias.nombre, categorias.nombre, multimedias.calificacion, sinopsis, premiosMultimedia.nombre;
 
-/*Consultar los actores principales de las peliculas con mejor calificacion de cierta categoria*/
-SELECT multimedias.nombre, sinopsis, calificacion, actores.nombre, apellido, sexo
-FROM peliculas, multimedias, actua, actores
-WHERE peliculas.idMultimedia = multimedias.id AND multimedias.id = actua.idMultimedia AND actores.id = actua.idActor AND personaje = 'principal' AND calificacion >= 4
-GROUP BY multimedias.nombre, sinopsis, calificacion, actores.nombre, apellido, sexo;
+/*Como productor quiero consultar los actores principales de las peliculas con mejor calificacion de la categoria de comedias para saber que opciones tengo de protagonistas
+    en mi proxima super produccion*/
+SELECT multimedias.nombre, sinopsis, calificacion, actores.nombre, apellido, sexo, categorias.nombre
+FROM peliculas, multimedias, actua, actores, categorias
+WHERE peliculas.idMultimedia = multimedias.id AND multimedias.id = actua.idMultimedia AND actores.id = actua.idActor AND categorias.idMultimedia = multimedias.id
+        AND personaje = 'principal' AND calificacion >= 4 AND categorias.nombre = 'Comedies'
+GROUP BY multimedias.nombre, sinopsis, calificacion, actores.nombre, apellido, sexo, categorias.nombre;
