912a07ea8c224065ffa58996878fc975412da904
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-15 22:11:11 -0500

    correccion tablas

--- Entrega Final/Tablas.sql
+++ Entrega Final/Tablas.sql
@@ -10,53 +10,46 @@ CREATE TABLE Categorias(id NUMBER NOT NULL,
 						nombre VARCHAR(100) NOT NULL);
 
 CREATE TABLE CategoriasMultimedias(idCategoria NUMBER NOT NULL,
-									idMultimedia NUMBER NOT NULL);
-
-CREATE TABLE InfoPremiosMultimedia(id NUMBER NOT NULL,
-								nombre VARCHAR(200) NOT NULL,
-								categoriaPremio VARCHAR(50) NOT NULL);									
+									idMultimedia NUMBER NOT NULL);							
 									
-CREATE TABLE PremiosMultimedia(idPremio NUMBER NOT NULL,
+CREATE TABLE PremiosMultimedia(id NUMBER NOT NULL,
+							nombre VARCHAR(200) NOT NULL,
+							categoriaPremio VARCHAR(50) NOT NULL,
 							idMultimedia NUMBER NOT NULL);
 			
 CREATE TABLE Series(id NUMBER NOT NULL);
 
 CREATE TABLE Temporadas(id NUMBER NOT NULL,
-						idSerie NUMBER NOT NULL);
-						
-CREATE TABLE InfoTemporadas(idTemporada NUMBER NOT NULL,
-							fechaEstreno DATE NOT NULL,
-							numeroTemporada NUMBER NOT NULL);
+						idSerie NUMBER NOT NULL,
+						fechaEstreno DATE NOT NULL,
+						numeroTemporada NUMBER NOT NULL);
 
 CREATE TABLE CapitulosSeries(id NUMBER NOT NULL,
-							idTemporada NUMBER NOT NULL);
-							
-CREATE TABLE InfoCapitulosSeries(idCapitulo NUMBER NOT NULL,
-								nombre VARCHAR(300) NOT NULL,
-								duracion VARCHAR(20) NOT NULL,
-								descripcion VARCHAR(2000) NOT NULL,
-								fechaEstreno DATE NOT NULL,
-								numeroCapitulo NUMBER NOT NULL);
+							idTemporada NUMBER NOT NULL,
+							idSerie NUMBER NOT NULL,
+							nombre VARCHAR(300) NOT NULL,
+							duracion VARCHAR(20) NOT NULL,
+							descripcion VARCHAR(2000) NOT NULL,
+							fechaEstreno DATE NOT NULL,
+							numeroCapitulo NUMBER NOT NULL);
 							
 CREATE TABLE Documentales(id NUMBER NOT NULL);
 							
 CREATE TABLE CapitulosDocumentales(id NUMBER NOT NULL,
-									idDocumental NUMBER NOT NULL);
-									
-CREATE TABLE InfoCapitulosDocumentales(idCapitulo NUMBER NOT NULL,
-										nombre VARCHAR(300) NOT NULL,
-										duracion VARCHAR(20) NOT NULL,
-										descripcion VARCHAR(2000) NOT NULL,
-										fechaEstreno DATE NOT NULL,
-										numeroCapitulo NUMBER NOT NULL);
+									idDocumental NUMBER NOT NULL,
+									nombre VARCHAR(300) NOT NULL,
+									duracion VARCHAR(20) NOT NULL,
+									descripcion VARCHAR(2000) NOT NULL,
+									fechaEstreno DATE NOT NULL,
+									numeroCapitulo NUMBER NOT NULL);
  
 CREATE TABLE Directores(id NUMBER NOT NULL,
 						nombre VARCHAR(300) NOT NULL,
 						apellido VARCHAR(300) NOT NULL,
 						calificacion NUMBER(5,1) NOT NULL);
 						
-CREATE TABLE Actua(idMultimedia NUMBER NOT NULL,
-					idActor NUMBER NOT NULL,
+CREATE TABLE Actua(idActor NUMBER NOT NULL,
+					idMultimedia NUMBER NOT NULL,
 					personaje VARCHAR(20));
 					
 CREATE TABLE Actores(id NUMBER NOT NULL,
@@ -65,12 +58,10 @@ CREATE TABLE Actores(id NUMBER NOT NULL,
 					fechaNacimiento DATE NOT NULL,
 					fechaFallecimiento DATE,
 					sexo VARCHAR(9) NOT NULL);
-					
-CREATE TABLE InfoPremiosActores(id NUMBER NOT NULL,
-								nombre VARCHAR(300) NOT NULL,
-								categoriaPremio VARCHAR(100) NOT NULL);
 							
-CREATE TABLE PremiosActores(idPremio NUMBER NOT NULL,
+CREATE TABLE PremiosActores(id NUMBER NOT NULL,
+							nombre VARCHAR(300) NOT NULL,
+							categoriaPremio VARCHAR(100) NOT NULL,
 							idActor NUMBER NOT NULL);
 							
 CREATE TABLE Observa(idMultimedia NUMBER NOT NULL,
@@ -86,7 +77,7 @@ CREATE TABLE Plantillas(id NUMBER NOT NULL,
 CREATE TABLE Suscripciones(id NUMBER NOT NULL,
 						nombre VARCHAR(300) NOT NULL,
 						numeroPlantillas NUMBER(1) NOT NULL,
-						idUsuario NUMBER NOT NULL,
+						idUsuario VARCHAR(100) NOT NULL,
 						idPlan NUMBER NOT NULL,
 						activa NUMBER(1) NOT NULL);
 						
@@ -101,7 +92,7 @@ CREATE TABLE Solicitudes(id NUMBER NOT NULL,
 						titulo VARCHAR(300) NOT NULL);					
 					
 CREATE TABLE Solicita(idSolicitud NUMBER NOT NULL,
-					idUsuario NUMBER NOT NULL);
+					idUsuario VARCHAR(100) NOT NULL);
 
 CREATE TABLE Planes(id NUMBER NOT NULL,
 					nombre VARCHAR(300) NOT NULL);
