9d123c1dab8f124f12045adcb6d0d26877bdc066
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-16 17:28:24 -0500

    correccion

--- Entrega Final/Tuplas.sql
+++ Entrega Final/Tuplas.sql
@@ -1,4 +1,3 @@
 ALTER TABLE Multimedias ADD CONSTRAINT K_multimedia CHECK (calificacion >= 0 AND calificacion <= 5);
 ALTER TABLE Directores ADD CONSTRAINT K_director CHECK (calificacion >= 0 AND calificacion <= 5);
-ALTER TABLE Actores ADD CONSTRAINT K_actores CHECK (fechaNacimiento < fechaFallecimiento);
 ALTER TABLE Usuarios ADD CONSTRAINT K_usuarios CHECK (correo LIKE '%@%' AND correo LIKE '%.%');
