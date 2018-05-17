17f288695bd132330fe87bfd754f93fd85c33777
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-17 17:34:39 -0500

    se creo tuplasNoOK

---
+++ Entrega Final/TuplasNoOK.sql
@@ -0,0 +1,11 @@
+INSERT INTO directores (id,nombre,apellido,calificacion) VALUES(1004,'clementino','jimenez',-2);
+INSERT INTO directores (id,nombre,apellido,calificacion) VALUES(1005,'lindomar','hernandez',5.5);
+INSERT INTO directores (id,nombre,apellido,calificacion) VALUES(1006,'abelardo','castano',-0.1);
+
+INSERT INTO multimedias (id,nombre,calificacion,duracion,idDirector,sinopsis,fechaEstreno) VALUES(3003,'hasta que te conoci',-1.0,'1hora 34minutos',33,'Desste segmento para hablar de las llamadas tendencias de moda entre los jovenes tales como pipas de agua el vino el marketing de guerrilla y los videojuegos. El 22 de marzo de 2007 Demetri hizo otra aparicion ',TO_DATE('20-6-1990','DD-MM-YYYY'));
+INSERT INTO multimedias (id,nombre,calificacion,duracion,idDirector,sinopsis,fechaEstreno) VALUES(3004,'al otro lado del charco',8.5,'1hora 50minutos',34,'Su particular estilo divertido y animado lo ha convertido en uno de los comediantes mas exitosos con eventos con boletos agotados en todo el mundo inclusive agotando boletos en Madison Square Garden y el Microsoft Theater. Ademas es unos de los comediantes mas vistos en YouTube con mas de 300 millones de vistas',TO_DATE('29-8-2000','DD-MM-YYYY'));
+INSERT INTO multimedias (id,nombre,calificacion,duracion,idDirector,sinopsis,fechaEstreno) VALUES(3005,'always with me',6,'2horas 10minutos',35,'Cuenta la historia de dos jovenes que se conocieron en el High School y se volvieron muy cercanos al punto de enamorarse, pero muchos años despues sus padres los separan y la chica muere de una enfermedad y el chico sin saber esto sigue fiel a ese amor',TO_DATE('15-3-2012','DD-MM-YYYY'));
+
+INSERT INTO usuarios (correo,nombre,fechaNacimiento) VALUES('czwwrkzlqtjbexhotmail.com','abelard',TO_DATE('21-4-2000','DD-MM-YYYY'));
+INSERT INTO usuarios (correo,nombre,fechaNacimiento) VALUES('czwwrbexmucdvy@hotmailcom','josue',TO_DATE('6-4-1998','DD-MM-YYYY'));
+INSERT INTO usuarios (correo,nombre,fechaNacimiento) VALUES('tjbexmucdvyhotmailcom','lindo',TO_DATE('24-8-1995','DD-MM-YYYY'));
