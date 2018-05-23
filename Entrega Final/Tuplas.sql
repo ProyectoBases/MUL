87f5d806a0ca1528be2e68890c0caaf9b62506a0
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-23 01:06:27 -0500

    se añadieron tuplas

--- Entrega Final/Tuplas.sql
+++ Entrega Final/Tuplas.sql
@@ -1,3 +1,7 @@
 ALTER TABLE Multimedias ADD CONSTRAINT K_multimedia CHECK (calificacion >= 0 AND calificacion <= 5);
 ALTER TABLE Directores ADD CONSTRAINT K_director CHECK (calificacion >= 0 AND calificacion <= 5);
 ALTER TABLE Usuarios ADD CONSTRAINT K_usuarios CHECK (correo LIKE '%@%' AND correo LIKE '%.%');
+ALTER TABLE Multimedias ADD CONSTRAINT K_multimedia2 CHECK (duracion LIKE '%horas%minutos');
+ALTER TABLE CapitulosSeries ADD CONSTRAINT K_capseries CHECK (duracion LIKE '%horas%minutos');
+ALTER TABLE CapitulosDocumentales ADD CONSTRAINT K_capdocu CHECK (duracion LIKE '%horas%minutos');
+ALTER TABLE Planes ADD CONSTRAINT K_planes CHECK (costo > 0);
