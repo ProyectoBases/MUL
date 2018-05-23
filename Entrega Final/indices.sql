90e7cd0a052a8939653cc0c54c5700ca1701b259
AndresFelipeGualdron <gualdronsito@hotmail.com>
2018-05-22 20:36:28 -0500

    correccion

--- Entrega Final/indices.sql
+++ Entrega Final/indices.sql
@@ -1,6 +1,15 @@
-CREATE INDEX multi
+CREATE INDEX multiU
 ON multimedias(nombre);
+/
+CREATE INDEX multiD
+ON multimedias(fechaEstreno);
 /
 CREATE INDEX soli
 ON solicitudes(titulo);
+/
+CREATE INDEX obserM
+ON observa(idMultimedia);
+/
+CREATE INDEX obserP
+ON observa(idPlantilla);
 /
